4ec5b223a54a529e0b84af6fd9f540211872990a
bilofili <29082005fil@gmail.com>
2023-09-04 09:16:26 -0400

    new one

--- src/Component/Main/ThreeBlock.js
+++ src/Component/Main/ThreeBlock.js
@@ -6,24 +6,26 @@ import web_icon from './web-development.png'
 
 export const ThreeBlock = () => {
 
-    const [ref, inView] = useInView(true);
+    const [refOne, inViewOne] = useInView(true);
+    const [refTwo, inViewTwo] = useInView(true);
+    const [refThree, inViewThree] = useInView(true);
  
         
     return (
         <div className="three-block">
             <motion.div
-            ref={ref}  
+            ref={refOne}  
             initial={{ x: 60, y: 0, opacity: 0 }}
-            animate={inView ? {opacity: 1, x: 0, y: 0} : {opacity: 0, x: 0, y: 0 }}  
+            animate={inViewOne? {opacity: 1, x: 0, y: 0} : {opacity: 0, x: 60, y: 0 }}  
             transition={{ duration: 1 }}
             className="block">
             <p>I have over 3 of experience in web development.</p>
             </motion.div>
 
             <motion.div 
-            ref={ref}
+            ref={refTwo}
             initial={{ x: 60, y: 0, opacity: 0 }}
-            animate={inView ? {opacity: 1, x: 0, y: 0} : {opacity: 0, x: 0, y: 0 }}  
+            animate={inViewTwo ? {opacity: 1, x: 0, y: 0} : {opacity: 0, x: 60, y: 0 }}  
             transition={{ duration: 1.3 }}
             className="block">
             <p>My specialization is in developing custom websites using modern technologies.</p>
@@ -31,9 +33,9 @@ export const ThreeBlock = () => {
             </motion.div>
 
             <motion.div
-            ref={ref}
+            ref={refThree}
             initial={{ x: 60, y: 0, opacity: 0 }}
-            animate={inView ? {opacity: 1, x: 0, y: 0} : {opacity: 0, x: 0, y: 0 }}  
+            animate={inViewThree ? {opacity: 1, x: 0, y: 0} : {opacity: 0, x: 60, y: 0 }}  
             transition={{ duration: 1.6 }}
             className="block">
             <p>I have worked on a variety of projects, from small business websites to large web applications.</p>
