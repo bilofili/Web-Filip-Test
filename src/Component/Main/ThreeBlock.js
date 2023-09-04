b921459602e52ff7025953d1cdc4cb95b2ad6661
bilofili <29082005fil@gmail.com>
2023-09-04 09:09:48 -0400

    new one updata

--- src/Component/Main/ThreeBlock.js
+++ src/Component/Main/ThreeBlock.js
@@ -6,39 +6,37 @@ import web_icon from './web-development.png'
 
 export const ThreeBlock = () => {
 
-    const [ref, inView] = useInView({
-        triggerOnce: true, // Анимация будет срабатывать только один раз, когда блок становится видимым
-    });
+    const [ref, inView] = useInView(true);
  
         
     return (
         <div className="three-block">
             <motion.div
             ref={ref}  
-            initial={{ x: 120, y: 0, opacity: 0 }}
+            initial={{ x: 60, y: 0, opacity: 0 }}
             animate={inView ? {opacity: 1, x: 0, y: 0} : {opacity: 0, x: 0, y: 0 }}  
-            transition={{ duration: 1.5 }}
+            transition={{ duration: 1 }}
             className="block">
             <p>I have over 3 of experience in web development.</p>
             </motion.div>
 
             <motion.div 
             ref={ref}
-            initial={{ x: 120, y: 0, opacity: 0 }}
+            initial={{ x: 60, y: 0, opacity: 0 }}
             animate={inView ? {opacity: 1, x: 0, y: 0} : {opacity: 0, x: 0, y: 0 }}  
-            transition={{ duration: 2 }}
+            transition={{ duration: 1.3 }}
             className="block">
             <p>My specialization is in developing custom websites using modern technologies.</p>
+            <img src={web_icon} alt="experienc" className='webIcon' />
             </motion.div>
 
             <motion.div
             ref={ref}
-            initial={{ x: 120, y: 0, opacity: 0 }}
+            initial={{ x: 60, y: 0, opacity: 0 }}
             animate={inView ? {opacity: 1, x: 0, y: 0} : {opacity: 0, x: 0, y: 0 }}  
-            transition={{ duration: 2.5 }}
+            transition={{ duration: 1.6 }}
             className="block">
             <p>I have worked on a variety of projects, from small business websites to large web applications.</p>
-            <img src={web_icon} alt="experienc" className='webIcon' />
             </motion.div>
 
         </div>
